1c78f838c64fcd78ffe861a7d2199519a9744836
mookyoseo <mookyo.seo@gmail.com>
2016-11-22 02:37:12 +0900

    initial commit

---
+++ src/main/java/kr/co/treasurehunter/JwpApplication.java
@@ -0,0 +1,12 @@
+package kr.co.treasurehunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpApplication.class, args);
+	}
+}
